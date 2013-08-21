d4fc6cd1d1ef424ad9647b0fe313e32d1ace69d4
Jim Kinsey <jim.kinsey@springer.com>
2013-08-21 17:27:16 +0100

    Support for named class parameters

--- src/main/scala/Builder.scala
+++ src/main/scala/Builder.scala
@@ -3,15 +3,36 @@ package com.github.jimkinsey.builder
 import scala.reflect.runtime.universe._
 import scala.reflect.runtime._
 
-class Builder[T: TypeTag]() {
+class Builder[T: TypeTag](private val fields: Map[String,Any] = Map()) extends Dynamic {
+
+	import scala.language.dynamics
 
 	def build: T = {
-		tConstructor().asInstanceOf[T]
+		val values = tFields.map(v => fields(v.name.decoded))		
+		tConstructor(values:_*).asInstanceOf[T]
 	}
 	
+	def applyDynamic(name: String)(args: Any*) = name match {
+		case WithOrAnd(_, field) => new Builder[T](fields + (decapitalise(field) -> args(0)))
+	}
+	
+	private def decapitalise(str: String) = str match {
+		case str if str.length > 1 => str(0).toString.toLowerCase + str.substring(1)
+		case str if str.length == 1 => str.toLowerCase
+		case _ => str
+	}
+	
+	private lazy val WithOrAnd = """(with|and)([A-Za-z]+)""".r
+		
 	private lazy val tType = universe.typeOf[T]
 	
 	private lazy val tClass = tType.typeSymbol.asClass
+	
+	private lazy val tFields = tType.declarations.sorted.filter { symbol => symbol match {
+			case symbol: MethodSymbol => symbol.isParamAccessor
+			case _ => false
+		}
+	}
 		
 	private lazy val tConstructor = {
 		val mirror = universe.runtimeMirror(getClass.getClassLoader)
--- src/test/scala/BuilderTests.scala
+++ src/test/scala/BuilderTests.scala
@@ -13,7 +13,29 @@ class BuilderTests extends FunSpec with ShouldMatchers {
 			val builtNoFields = noFieldsBuilder.build
 			builtNoFields should equal(NoFields())
 		}	
+		
+		it("should set a field specified using the with prefix") {
+			val builderWithField = new Builder[OneNameField].withName("Inego")
+			val inego = builderWithField.build
+			inego should equal(new OneNameField("Inego"))
+		}
+		
+		it("should set two fields specified using the with and and prefixes") {
+			val builderWithTwoFields = new Builder[TwoNameFields].withFirstName("Inego").andLastName("Montoya")
+			val inegoMontoya = builderWithTwoFields.build
+			inegoMontoya should equal(new TwoNameFields("Inego", "Montoya"))
+		}
+		
+		it("should set two fields specified using the with and and prefixes regardless of order") {
+			val builderWithTwoFields = new Builder[TwoNameFields].withLastName("Montoya").andFirstName("Inego")
+			val inegoMontoya = builderWithTwoFields.build
+			inegoMontoya should equal(new TwoNameFields("Inego", "Montoya"))
+		}
 	}
 }
 
 case class NoFields
+
+case class OneNameField(name: String)
+
+case class TwoNameFields(firstName: String, lastName: String)
